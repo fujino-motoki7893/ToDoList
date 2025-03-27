13dc5bff97db58d9f733879b4ba1ed693f95efd2
fujino-motoki22-fixer <fujino.motoki22@fixer.co.jp>
2025-03-27 12:20:33 +0900

    🎉 VSCode設定ファイルを追加し、ESLintルールをカスタマイズしました

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
-// @ts-check
+import stylistic from '@stylistic/eslint-plugin'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  // プロジェクトのコーディングルールを追加する
+  {
+    files: ['**/*.vue', '**/*.ts'],
+    rules: {
+      // コードの長さを1行あたり120文字以内に制限
+      'max-len': ['error', { code: 120 }],
+      // 複数の連続した空行を禁止し、最大1行とする
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+    },
+  },
+  // フォーマッタのカスタムルール
+  stylistic.configs.customize({
+    indent: 2, // インデントはスペース2
+  }),
 )
