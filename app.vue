b8fb087115f8e42877c97e836f64bd0fba7e2e63
fujino-motoki22-fixer <fujino.motoki22@fixer.co.jp>
2025-03-27 11:29:12 +0900

    🎉 カスタムレイアウトを追加し、ページに適用しました

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout :name="layout">
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+// You might choose this based on an API call or logged-in status
+const layout = "custom";
+</script>
