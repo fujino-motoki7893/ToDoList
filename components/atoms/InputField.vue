a6b073519699276caefe0cfade425ad1dde03a8e
fujino-motoki7893 <motoki.fujino7893@gmail.com>
2025-04-03 15:59:53 +0900

    🎉 InputFieldコンポーネントから不要なtextareaとフォーカス制御を削除しました。 (#17)
    Co-authored-by: fujino-motoki22-fixer <fujino.motoki22@fixer.co.jp>

--- components/atoms/InputField.vue
+++ components/atoms/InputField.vue
@@ -18,22 +18,6 @@
     :placeholder="placeholder"
     :disabled="props.isDisabled"
   >
-  <div
-    v-else
-    class="input-field"
-  >
-    <textarea
-      v-model="model"
-      :placeholder="placeholder"
-      :disabled="props.isDisabled"
-      @focus="onFocus"
-      @blur="onBlur"
-    />
-    <CancelButton
-      v-if="clearable && !props.isDisabled && model"
-      @click="onClickCancelButton"
-    />
-  </div>
 </template>
 
 <script setup lang="ts">
@@ -52,9 +36,4 @@ onMounted(() => {
     model.value = props.defaultValue
   }
 })
-
-// textarea時のフォーカス制御
-const isFocused = ref<boolean>(false)
-const onFocus = () => isFocused.value = true
-const onBlur = () => isFocused.value = false
 </script>
