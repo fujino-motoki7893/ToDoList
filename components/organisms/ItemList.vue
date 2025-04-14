f3795c733559cf4ae8ffadef4a06dcbfda5bea7a
fujino-motoki22-fixer <fujino.motoki22@fixer.co.jp>
2025-04-14 20:09:46 +0900

    🎉 ItemListコンポーネントを更新し、ToDoリストの表示を強化しました。

--- components/organisms/ItemList.vue
+++ components/organisms/ItemList.vue
@@ -3,15 +3,34 @@
     <TableHeaderWrapper
       class="bg-gray-200 p-4 text-lg font-bold"
     >
-      ItemListのヘッダーです。
+      ToDoList
     </TableHeaderWrapper>
-    <TableRowWrapper>
-      ItemListの内容です。
+    <TableRowWrapper
+      v-for="item in items"
+      :key="item.id"
+    >
+      <div class="flex items-center justify-between p-4 border-b border-gray-300">
+        <div class="flex items-center">
+          <input
+            type="checkbox"
+            class="mr-2"
+          >
+          <span>{{ item.id }}.  </span>
+          <span>{{ item.name }}： </span>
+          <span>{{ item.content }}</span>
+        </div>
+      </div>
     </TableRowWrapper>
   </div>
 </template>
 
 <script setup lang="ts">
+import { mockData } from '@/repositories/domains/getItems'
+
+// 繋ぎ込んでいない状態の動作確認のため、モックデータを使用
+const items = ref(mockData)
+console.log(items.value)
+
 onMounted(async () => {
   try {
     const data = await $fetch('/graphql', {
