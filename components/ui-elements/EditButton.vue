ca5bc521d72eab27f808dc7a0f089c9926a20db0
fujino-motoki7893 <motoki.fujino7893@gmail.com>
2025-05-14 16:21:23 +0900

    🔨 アイテム編集モード実装 (#29)
    Co-authored-by: fujino-motoki22-fixer <fujino.motoki22@fixer.co.jp>

--- components/ui-elements/EditButton.vue
+++ components/ui-elements/EditButton.vue
@@ -1,7 +1,11 @@
 <template>
-  <button class="p-1 text-gray-500 hover:text-indigo-600 transition-colors">
+  <button
+    class="p-1 text-gray-500 hover:text-indigo-600 transition-colors"
+    title="編集"
+    @click="$emit('edit')"
+  >
     <svg
-      xmlns="http://www.w3.org/2000/svg  "
+      xmlns="http://www.w3.org/2000/svg"
       class="h-5 w-5"
       fill="none"
       viewBox="0 0 24 24"
@@ -16,3 +20,7 @@
     </svg>
   </button>
 </template>
+
+<script setup lang="ts">
+defineEmits(['edit'])
+</script>
