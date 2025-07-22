b5c951fba09103946b467950def45bca37d22f93
fujino-motoki7893 <motoki.fujino7893@gmail.com>
2025-07-22 18:18:44 +0900

    📸 ポートフォリオの写真部分を追加 (#38)
    Co-authored-by: fujino-motoki22-fixer <fujino.motoki22@fixer.co.jp>

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,9 @@
         <Header />
       </header>
     </div>
+    <div class="flex justify-center">
+      <img src="~/assets/image/think_fujino_edit.jpg">
+    </div>
     <div class="max-w-md mx-auto p-6 bg-white rounded-lg shadow-lg">
       <h1 class="text-2xl font-bold text-center mb-6 text-indigo-600">
         ToDoList
