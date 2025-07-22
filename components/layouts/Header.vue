5575a25015e09a3fa80782f621ccbf8c4311287f
fujino-motoki22-fixer <fujino.motoki22@fixer.co.jp>
2025-07-22 17:55:13 +0900

    🎨 ポートフォリオのヘッダを追加

---
+++ components/layouts/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full h-[72px] shadow flex justify-center">
+    <span class="flex w-[1280px] items-center font-bold justify-between">
+      <span class="text-[36px]">
+        motokifujino.com
+      </span>
+      <nav class="text-[20px] text-gray-600 space-x-12">
+        <a
+          href="/"
+          class="hover:text-blue-600"
+        >HOME</a>
+        <a class="w-[1px] h-[24px] x-6 bg-gray-300 absolute mx-[-24px] my-[4px]" />
+        <a
+          href="/about"
+          class="hover:text-blue-600"
+        >ABOUT ME</a>
+      </nav>
+    </span>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="max-w-md mx-auto p-6 bg-white rounded-lg shadow-lg">
-    <h1 class="text-2xl font-bold text-center mb-6 text-indigo-600">
-      ToDoList
-    </h1>
+  <div>
+    <div>
+      <header>
+        <Header />
+      </header>
+    </div>
+    <div class="max-w-md mx-auto p-6 bg-white rounded-lg shadow-lg">
+      <h1 class="text-2xl font-bold text-center mb-6 text-indigo-600">
+        ToDoList
+      </h1>
 
-    <ItemList class="mb-8" />
+      <ItemList class="mb-8" />
 
-    <div class="space-y-4">
-      <ItemInput />
+      <div class="space-y-4">
+        <ItemInput />
+      </div>
     </div>
   </div>
 </template>
