fb94945a1f3e31e4579fdce324f2d5e00b34b901
fujino-motoki22-fixer <fujino.motoki22@fixer.co.jp>
2025-07-22 19:09:01 +0900

    ✨ ポートフォリオのボディ部分を追加

--- pages/index.vue
+++ pages/index.vue
@@ -5,25 +5,13 @@
         <Header />
       </header>
     </div>
-    <div class="flex justify-center">
-      <img src="~/assets/image/think_fujino_edit.jpg">
-    </div>
-    <div class="max-w-md mx-auto p-6 bg-white rounded-lg shadow-lg">
-      <h1 class="text-2xl font-bold text-center mb-6 text-indigo-600">
-        ToDoList
-      </h1>
-
-      <ItemList class="mb-8" />
-
-      <div class="space-y-4">
-        <ItemInput />
-      </div>
+    <div>
+      <Home />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import ItemList from '~/components/layouts/ItemList.vue'
 import { provideItemList } from '~/composables/domains/itemList'
 
 provideItemList()
