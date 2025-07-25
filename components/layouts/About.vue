32845f364b55bedeee81f1c14cadd6a7fd990e92
fujino-motoki22-fixer <fujino.motoki22@fixer.co.jp>
2025-07-25 11:45:33 +0900

    💄 フォントサイズを調節した

--- components/layouts/About.vue
+++ components/layouts/About.vue
@@ -43,13 +43,13 @@
         </div>
       </div>
     </div>
-    <div class="flex h-[622px]">
+    <div class="flex h-[566px]">
       <div class="text-[40px] w-[418px] flex justify-center font-bold px-16">
         たいせつなこと
       </div>
-      <div class="flex flex-col w-[1438px] leading-[1.67] tracking-[4px] pr-16">
-        <div class="text-[24px] font-bold">
-          <span class="flex">
+      <div class="flex flex-col w-[1438px] leading-[1.67] tracking-[4px] pr-16 text-[20px]">
+        <div class="font-bold">
+          <span class="flex text-[24px]">
             1. 共同が生み出す、一人を超えた価値
             <span class="px-2">
               <MessageSquare
@@ -58,13 +58,13 @@
               />
             </span>
           </span>
-          <p class="font-normal p-5">
+          <p class="font-normal py-6">
             多様な視点を持つ仲間と敬意をもって議論を交わし、互いの知見を掛け合わせるプロセスこそ、質の高いプロダクト開発に不可欠だと考えています。私が持つ知識も誰かから教わったものであり、
             それを次の誰かに繋いでいく。そんな知識の好循環を生み出すことで、チーム全体の力を高めることに貢献します。
           </p>
         </div>
-        <div class="text-[24px] font-bold">
-          <span class="flex">
+        <div class="font-bold">
+          <span class="flex text-[24px]">
             2. 「誰かのため」という実感が生む、当事者意識
             <span class="px-2">
               <UserRound
@@ -73,13 +73,13 @@
               />
             </span>
           </span>
-          <p class="font-normal p-5">
+          <p class="font-normal py-6">
             自分が書いたコードが、どのようにユーザーの課題を解決し、事業の成長に貢献しているのか。その繋がりを明確に感じられることに、私は最大のやりがいを感じます。人づてにでも「ありがとう」と感謝される喜びが、
             私を「もっと良くしたい」という次なる行動へと駆り立てます。常に目的意識と当事者意識を持ち、ユーザーとビジネスに寄り添った本質的な価値提供を追求します。
           </p>
         </div>
-        <div class="text-[24px] font-bold">
-          <span class="flex">
+        <div class="font-bold">
+          <span class="flex text-[24px]">
             3. 知的好奇心が生む、未来への挑戦
             <span class="px-2">
               <Lightbulb
@@ -88,7 +88,7 @@
               />
             </span>
           </span>
-          <p class="font-normal p-5">
+          <p class="font-normal py-6">
             「この技術を使えば、もっとスマートな解決ができるのでは」と考えるプロセスそのものに、大きな喜びを感じます。現在は特にクラウド技術（AWS）に関心があり、
             スケーラブルで堅牢なシステムアーキテクチャの設計・運用に挑戦することで、プロダクトの価値をさらに高めていきたいと考えています。
           </p>
@@ -96,35 +96,32 @@
       </div>
     </div>
     <div class="p-16" />
-    <div class="flex h-100">
+    <div class="flex">
       <div class="text-[40px] w-[418px] flex justify-center font-bold px-16">
         技術スタック
       </div>
-      <div class="flex flex-col w-[1438px] pr-16 text-[24px] tracking-[2px]">
-        <p class="font-normal pb-5">
-          言語、フレームワーク、ツール、設計思想などをごちゃまぜにしちゃっています。見にくかったらすみません
-        </p>
+      <div class="flex flex-col w-[1438px] pr-16 pt-4 text-[20px] tracking-[2px]">
         <div class="flex">
-          <span class="font-bold w-[198px]">
+          <span class="font-bold w-[198px] text-[24px]">
             フロントエンド...
           </span>
-          <p class="pb-5 w-[1220px]">
+          <p class="pb-8 w-[1220px]">
             TypeScript, HTML/CSS, Nuxt.js, Vue.js, pnpm,  codegen, Tailwind.css, Figma, 単一責任の原則、アトミックデザイン（縛られ過ぎない程度に）
           </p>
         </div>
         <div class="flex">
-          <span class="font-bold w-[198px]">
+          <span class="font-bold w-[198px] text-[24px]">
             バックエンド...
           </span>
-          <p class="pb-5 w-[1220px]">
+          <p class="pb-8 w-[1220px]">
             C#, ASP.NET Core, GraphQL, REST, Swagger, Nitro, クリーンアーキテクチャ、Repository pattern, Jet Brains Rider
           </p>
         </div>
         <div class="flex">
-          <span class="font-bold w-[198px]">
+          <span class="font-bold w-[198px] text-[24px]">
             データベース・<br>インフラ...
           </span>
-          <p class="pb-5 w-[1220px]">
+          <p class="pb-8 w-[1220px]">
             GraphQL, PostgreSQL, A5:SQL Mk-2, DBeaver, Azure（DevOps Services, Front Door, SQL Database,
             Virtual Machines, Container Registry, Kubernetes Services, Functions）, AWS（Lambda, Cloud Watch,
             S3, Cloudfront, API Gateway,RDS,DynamoDB） , GitOps, GitHub, Grafana, Google Analytics,
